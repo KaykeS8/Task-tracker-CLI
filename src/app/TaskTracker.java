734e9f103c777f6566d6656ac7bb8e4498015a67
Kayke simao <kayke.py@gmail.com>
2026-03-03 22:39:22 -0300

    init code

--- src/app/TaskTracker.java
+++ src/app/TaskTracker.java
@@ -1,16 +1,12 @@
 package app;
 
 import cli.TaskCLI;
-import entities.Task;
 import repository.TaskRepository;
 import service.TaskService;
 import utils.TaskException;
 
-import java.io.IOException;
-
 public class TaskTracker {
     public static void main(String[] args) {
-
         try {
             TaskRepository repository  = new TaskRepository();
             TaskService service = new TaskService(repository);
