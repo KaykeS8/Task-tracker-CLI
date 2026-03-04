bd9093349b729a9ede29cae85d1af9c64f4cfc51
Kayke simao <kayke.py@gmail.com>
2026-03-03 22:19:54 -0300

    Class responsible for create CLI

--- src/cli/TaskCLI.java
+++ src/cli/TaskCLI.java
@@ -1,23 +1,48 @@
 package cli;
 
+import entities.Task;
 import service.TaskService;
 
 public class TaskCLI {
 
-    private TaskService service;
+    private final TaskService service;
 
     public TaskCLI(TaskService service) {
         this.service = service;
     }
 
     public void execute(String[] args) {
+        Task task;
         if (args.length == 0) {
             System.out.println("Invalid argument");
             return;
         }
 
         switch (args[0]) {
-            case "add" -> service.add(args[1]);
+            case "add":
+                task = service.add(args[1]);
+                System.out.println("Task added succefully (ID: " + task.getId() + ")");
+                break;
+
+            case "update":
+                task = service.update(Integer.parseInt(args[1]), args[2]);
+                System.out.println("Task updated with succefully (ID: ) " + task.getId() + ")");
+                break;
+
+            case "remove":
+                service.remove(Integer.parseInt(args[1]));
+                System.out.println("Task destroyed with succefully");
+                break;
+
+            case "mark-in-progress", "mark-done":
+                service.changeStatus(args[0], Integer.parseInt(args[1]));
+                break;
+
+            case "list":
+                service.all();
+                break;
+            default:
+                System.out.println("invalid argument");
         }
     }
 }
