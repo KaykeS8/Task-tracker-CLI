25ececfeb758cf6c9291e335dbada0ffa4423851
Kayke simao <kayke.py@gmail.com>
2026-03-03 22:21:28 -0300

    Enum task

--- src/utils/EnumTask.java
+++ src/utils/EnumTask.java
@@ -3,5 +3,14 @@ package utils;
 public enum EnumTask {
     TODO,
     IN_PROGRESS,
-    DONE
+    DONE;
+
+    public static String statusToString(EnumTask status) {
+        return switch (status) {
+            case TODO -> "todo";
+            case IN_PROGRESS -> "in-progress";
+            case DONE -> "done";
+            default -> "invalid status";
+        };
+    }
 }
